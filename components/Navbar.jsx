6e70ef5332de12bf918cb01d41ecea9628cd143f
Arturo Quirós <joseq2408@gmail.com>
2022-07-30 17:27:08 -0600

    css added to nav

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,9 @@
-import React from "react";
 import Link from "next/link";
+import styles from "./Navbar.module.css";
 
 export const Navbar = () => {
   return (
-    <nav>
+    <nav className={styles["menu-container"]}>
       <Link href="/">Home</Link>
       <Link href="/about">About</Link>
       <Link href="/contact">Contact</Link>

9e79ba7ebf46e36b1812116b8b01a2521f8a4353
Arturo Quirós <joseq2408@gmail.com>
2022-07-30 19:43:56 -0600

    final fixes

--- pages/about.tsx
+++ pages/about.tsx
@@ -12,7 +12,7 @@ export default function AboutPage() {
   );
 }
 
-AboutPage.getLayout = function getLayout(page) {
+AboutPage.getLayout = function getLayout(page: JSX.Element) {
   return (
     <MainLayout>
       <DarkLayout>{page}</DarkLayout>

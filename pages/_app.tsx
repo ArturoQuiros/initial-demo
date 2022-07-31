5674c725a20f4b6e02eaddad2ae8357bd68dc539
Arturo Quirós <joseq2408@gmail.com>
2022-07-30 19:42:20 -0600

    Final TS Migration

---
+++ pages/_app.tsx
@@ -0,0 +1,20 @@
+import { NextPage } from "next";
+import { AppProps } from "next/app";
+import { ReactElement, ReactNode } from "react";
+import "../styles/globals.css";
+
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout || ((page) => page);
+
+  return getLayout(<Component {...pageProps} />);
+}
+
+export default MyApp;
